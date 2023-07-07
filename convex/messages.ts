cade85f1af17aa9726443d6e216a84bc90082b50
Ian Macartney <ianhmacartney@gmail.com>
2023-07-07 00:14:12 -0700

    arg validation

--- convex/messages.ts
+++ convex/messages.ts
@@ -2,35 +2,41 @@ import { internal } from "./_generated/api";
 import { internalMutation, mutation } from "./_generated/server";
 import { query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
+import { v } from "convex/values";
 
-export const list = query(async ({ db }): Promise<Doc<"messages">[]> => {
-  return await db.query("messages").collect();
+export const list = query({
+  handler: async ({ db }): Promise<Doc<"messages">[]> => {
+    return await db.query("messages").collect();
+  },
 });
 
-export const send = mutation(
-  async (
-    { db, scheduler },
-    { body, author }: { body: string; author: string }
-  ) => {
-    const message = { body, author };
-    await db.insert("messages", message);
+export const send = mutation({
+  args: { body: v.string(), author: v.string() },
+  handler: async ({ db, scheduler }, { body, author }) => {
+    // Send our message.
+    await db.insert("messages", { body, author });
 
-    if (message.body.endsWith("?")) {
-      const messages = await db.query("messages").collect();
+    if (body.indexOf("@gpt") !== -1) {
+      // Fetch the latest 10 messages to send as context.
+      // The default order is by creation time.
+      const messages = await db.query("messages").order("desc").take(10);
+      // Reverse the list so that it's in chronological order.
+      messages.reverse();
+      // Insert a message with a placeholder body.
       const messageId = await db.insert("messages", {
         author: "ChatGPT",
         body: "...",
       });
+      // Schedule an action that calls ChatGPT and updates the message.
       scheduler.runAfter(0, internal.openai.chat, { messages, messageId });
     }
-  }
-);
+  },
+});
 
-export const update = internalMutation(
-  async (
-    { db },
-    { messageId, body }: { messageId: Id<"messages">; body: string }
-  ) => {
+// Updates a message with a new body.
+export const update = internalMutation({
+  args: { messageId: v.id("messages"), body: v.string() },
+  handler: async ({ db }, { messageId, body }) => {
     await db.patch(messageId, { body });
-  }
-);
+  },
+});
