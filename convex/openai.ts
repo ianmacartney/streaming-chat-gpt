a6a4a75047009b106ef0d80c6778a2555e632c2b
Ian Macartney <ianhmacartney@gmail.com>
2023-10-26 20:38:23 -0700

    update openai to newest api

--- convex/openai.ts
+++ convex/openai.ts
@@ -12,37 +12,45 @@ export const chat = internalAction({
     const apiKey = process.env.OPENAI_API_KEY!;
     const openai = new OpenAI({ apiKey });
 
-    const stream = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
-      stream: true,
-      messages: [
-        {
-          role: "system",
-          content: "You are a terse bot in a group chat responding to q's.",
-        },
-        ...messages.map(({ body, author }) => ({
-          role:
-            author === "ChatGPT" ? ("assistant" as const) : ("user" as const),
-          content: body,
-        })),
-      ],
-    });
-    if (!stream.response.ok) {
-      await ctx.runMutation(internal.messages.update, {
-        messageId,
-        body: "OpenAI call failed: " + stream.response.statusText,
+    try {
+      const stream = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
+        stream: true,
+        messages: [
+          {
+            role: "system",
+            content: "You are a terse bot in a group chat responding to q's.",
+          },
+          ...messages.map(({ body, author }) => ({
+            role:
+              author === "ChatGPT" ? ("assistant" as const) : ("user" as const),
+            content: body,
+          })),
+        ],
       });
-    }
-    let body = "";
-    for await (const part of stream) {
-      if (part.choices[0].delta?.content) {
-        body += part.choices[0].delta.content;
-        // Alternatively you could wait for complete words / sentences.
-        // Here we send an update on every stream message.
+      let body = "";
+      for await (const part of stream) {
+        if (part.choices[0].delta?.content) {
+          body += part.choices[0].delta.content;
+          // Alternatively you could wait for complete words / sentences.
+          // Here we send an update on every stream message.
+          await ctx.runMutation(internal.messages.update, {
+            messageId,
+            body,
+          });
+        }
+      }
+    } catch (e) {
+      if (e instanceof OpenAI.APIError) {
+        console.error(e.status);
+        console.error(e.message);
         await ctx.runMutation(internal.messages.update, {
           messageId,
-          body,
+          body: "OpenAI call failed: " + e.message,
         });
+        console.error(e);
+      } else {
+        throw e;
       }
     }
   },
